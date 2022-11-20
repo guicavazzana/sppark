834aa5717caf5c8ac3d371928f54e6394b53bb49
Guilherme Cavazzana <cavazzanagui@gmail.com>
2022-11-20 18:10:52 -0300

    Font-Family Changed

--- src/styles/global.js
+++ src/styles/global.js
@@ -13,7 +13,8 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0px;
         padding: 0px;
         box-sizing: border-box;
-        font-family: "Helvetica Neue",Helvetica;
+        font-family: 'Roboto', sans-serif;
+
     }
 
     html{
